06a820f2df9b107ebafc92d20952a945ac28654f
PolarNorth <antonov9312@yandex.ru>
2018-10-28 13:25:08 +0300

    derived plot in separate class PlotBox

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from kivy.properties import (
 )
 import matplotlib.pyplot as plt
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+from backend import *
 
 class MainApp (App):
     def build(self):
@@ -24,15 +25,42 @@ class MainScreen (Screen):
     inp_n = ObjectProperty(None)
     inp_xn = ObjectProperty(None)
     submit_btn = ObjectProperty(None)
-    graph_box = ObjectProperty(None)
     title_label = ObjectProperty(None)
+    plot_box_widget = ObjectProperty(None)
 
     def __init__(self, *args, **kwargs):
         super(Screen, self).__init__(*args, **kwargs)
-        initial_plot = ([0,1],[0,1])
-        plt.plot(*initial_plot)
+        self.current_de = equations[0]
+        self.plot_box_widget.set_plot(ImprovedEulerMethod.solve(self.current_de, 2, 0, 1, 0.1))
+        self.submit_btn.on_release = self.plot_box_widget.show_plot
+
+class PlotBox (BoxLayout):
+    equation_lbl = ObjectProperty(None)
+    show_diff_equation_btn = ObjectProperty(None)
+    show_numerical_solution_btn = ObjectProperty(None)
+    show_exact_solution_btn = ObjectProperty(None)
+    show_truncation_error_btn= ObjectProperty(None)
+    figure_box = ObjectProperty(None)
+
+    def __init__(self, *args, **kwargs):
+        super(BoxLayout, self).__init__(*args, **kwargs)
+        self.plot = None
+        self.plot_coordinates = None
+        # initial_plot = ([0,1],[0,1])
+        # plt.plot(*initial_plot)
+        # self.plot = FigureCanvasKivyAgg(plt.gcf())
+        # self.figure_box.add_widget(self.plot)
+    
+    def set_plot(self, solution):
+        self.plot_coordinates = (solution.x_solution, solution.y_solution)
+
+    def show_plot(self):
+        # initial_plot = ([0,1],[0,1])
+        plt.plot(*self.plot_coordinates)
         self.plot = FigureCanvasKivyAgg(plt.gcf())
-        self.graph_box.add_widget(self.plot)
+        self.figure_box.clear_widgets()
+        self.figure_box.add_widget(self.plot)
+
 
 if __name__ == '__main__':
     MainApp().run()
